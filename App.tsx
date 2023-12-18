a8e9dc29358e7fe9ef80b5f2048a9c2037050e48
jia-son <116625758+jia-son@users.noreply.github.com>
2023-12-19 05:02:24 +0900

    모당찰 사용

--- App.tsx
+++ App.tsx
@@ -18,18 +18,20 @@ import {
 import {theme} from './colors';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Modal from 'react-native-modal';
 
 const STORAGE_KEY = '@toDos';
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState('');
-  // const [toDos, setToDos] = useState<{
-  //   [key: string]: {text: string; working: boolean};
-  // }>({});
   const [toDos, setToDos] = useState<{
     [key: string]: {text: string; working: boolean; done: boolean};
   }>({});
+
+  const [isModal, setIsModal] = useState(false);
+  const [updateText, setUpdateText] = useState('');
+  const [selectKey, setSelectKey] = useState('');
 
   useEffect(() => {
     loadToDos();
@@ -58,8 +60,24 @@ export default function App() {
     }
   };
 
+  const toggleModal = () => {
+    setIsModal(!isModal);
+  };
+
   const updateToDo = (key: string) => {
-    Alert.alert('Update To Do');
+    setSelectKey(key);
+    toggleModal();
+  };
+
+  const handleUpdate = () => {
+    const newToDos = {
+      ...toDos,
+      [selectKey]: {...toDos[selectKey], text: updateText},
+    };
+
+    setToDos(newToDos);
+    saveToDos(newToDos);
+    toggleModal();
   };
 
   const deleteToDo = (key: string) => {
@@ -172,6 +190,20 @@ export default function App() {
           ) : null,
         )}
       </ScrollView>
+      {/*모달창*/}
+      <Modal isVisible={isModal} onBackdropPress={toggleModal}>
+        <View style={styles.modalContent}>
+          <Text style={styles.modalTitle}>Edit To Do</Text>
+          <TextInput
+            style={styles.modalInput}
+            onChangeText={text => setUpdateText(text)}
+            value={updateText}
+          />
+          <TouchableOpacity onPress={handleUpdate}>
+            <Text>Update</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -223,5 +255,22 @@ const styles = StyleSheet.create({
   },
   toDoUpdateBtn: {
     paddingRight: 10,
+  },
+  modalContent: {
+    backgroundColor: theme.white,
+    padding: 22,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  modalInput: {
+    height: 70,
+    borderBottomWidth: 1,
+    marginBottom: 20,
+  },
+  modalTitle: {
+    fontSize: 20,
+    fontWeight: '600',
+    color: theme.grey,
   },
 });
